2a469a96270567b3fba479a95ce659a64e168df8
Fallen4522 <dako.kalmakhan.02@gmail.com>
2024-12-02 19:52:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
+
 // Корзина
 const cartCount = document.getElementById('cart-count');
 const cartList = document.getElementById('cart-list');
